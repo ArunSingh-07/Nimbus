f165799e8ba2979929afac8049360f2746424f4f
Arun Singh <arun987770@gmail.com>
2025-12-15 22:01:40 +0530

    playground implemented along with dropdown buttons

--- modules/dashboard/actions/index.ts
+++ modules/dashboard/actions/index.ts
@@ -2,6 +2,46 @@
 
 import { db } from "@/lib/db";
 import { currentUser } from "@/modules/auth/actions";
+import { revalidatePath } from "next/cache";
+
+export const toggledStarMarked = async (
+  playgroundId: string,
+  isChecked: boolean
+) => {
+  const user = await currentUser();
+
+  const userId = user?.id;
+
+  if (!userId) {
+    throw new Error("User id is required! ");
+  }
+
+  try {
+    if (isChecked) {
+      await db.starMark.create({
+        data: {
+          userId: userId,
+          playgroundId,
+          isMarked: isChecked,
+        },
+      });
+    } else {
+      await db.starMark.delete({
+        where: {
+          userId_playgroundId: {
+            userId: userId,
+            playgroundId,
+          },
+        },
+      });
+    }
+    revalidatePath("/dashboard");
+    return { success: true, isMarked: isChecked };
+  } catch (error) {
+    console.error("Error updating problem", error);
+    return { success: false, error: "failed to update" };
+  }
+};
 
 export const getAllPlaygroundForUser = async () => {
   const user = await currentUser();
@@ -13,11 +53,106 @@ export const getAllPlaygroundForUser = async () => {
       },
       include: {
         user: true,
+        Starmark: {
+          where: {
+            userId: user?.id!,
+          },
+          select: {
+            isMarked: true,
+          },
+        },
       },
     });
 
     return playground;
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const createPlayground = async (data: {
+  title: string;
+  template: "REACT" | "EXPRESS" | "VUE" | "HONO" | "ANGULAR" | "NEXTJS";
+  description?: string;
+}) => {
+  const user = await currentUser();
+
+  const { template, title, description } = data;
+
+  try {
+    const playground = await db.playground.create({
+      data: {
+        title: title,
+        description: description,
+        template: template,
+        userId: user?.id!,
+      },
+    });
+
+    return playground;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteProjectById = async (id: string) => {
+  try {
+    await db.playground.delete({
+      where: {
+        id,
+      },
+    });
+
+    revalidatePath("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProjectById = async (
+  id: string,
+  data: { title: string; description: string }
+) => {
+  try {
+    await db.playground.update({
+      where: {
+        id,
+      },
+      data: data,
+    });
+
+    revalidatePath("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const duplicateProjectById = async (id: string) => {
+  try {
+    const originalPlayground = await db.playground.findUnique({
+      where: { id },
+      // todo: add template files
+    });
+
+    if (!originalPlayground) {
+      throw new Error("Original playground not found");
+    }
+
+    const duplicatedPlayground = await db.playground.create({
+      data: {
+        title: `${originalPlayground.title} (copy)`,
+        description: originalPlayground.description,
+        template: originalPlayground.template,
+        userId: originalPlayground.userId,
+
+        // todo: add template files
+      },
+    });
+
+    revalidatePath("/dashboard");
+    return deleteProjectById;
+  } catch (error) {
+    console.log(error);
+    console.error("Error while duplicating Playground");
   }
 };
