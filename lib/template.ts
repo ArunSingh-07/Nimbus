665c78194175ec91166e781c2471d147618ffd02
Arun Singh <arun987770@gmail.com>
2025-12-16 23:24:45 +0530

    file explorer implementation 1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/framework-starters/react-ts",
+  NEXTJS: "/framework-starters/nextjs",
+  EXPRESS: "/framework-starters/express-simple",
+  VUE: "/framework-starters/vue",
+  HONO: "/framework-starters/hono-nodejs-starter",
+  ANGULAR: "/framework-starters/angular",
+};
