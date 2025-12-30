b962ae191e41b3d182e67dad1ad31472c81572d6
Arun Singh <arun987770@gmail.com>
2025-12-30 18:16:26 +0530

    setup of AI chat area

--- modules/playground/components/toggle-AI.tsx
+++ modules/playground/components/toggle-AI.tsx
@@ -35,6 +35,7 @@ import {
 } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { AIChatSidePanel } from "@/modules/ai-chat/components/ai-chat-sidebarpanel";
 // import { AIChatSidePanel } from "@/features/ai-chat/components/ai-chat-sidepanel";
 
 interface ToggleAIProps {
@@ -195,6 +196,11 @@ const ToggleAI: React.FC<ToggleAIProps> = ({
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+
+      <AIChatSidePanel
+        isOpen={isChatOpen}
+        onClose={() => setIsChatOpen(false)}
+      />
     </>
   );
 };
